9c2dfbe0051a44c7a33680d7aa33e0cbb013bbb2
Daniel Feltey <dfeltey@gmail.com>
2014-10-22 10:09:35 -0400

    fix

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/check-unit-unit.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/check-unit-unit.rkt
@@ -221,9 +221,6 @@
        (with-lexical-env/extend 
            (map car all-annotations) (map cdr all-annotations)
            (for/last ([stx (in-list forms)])
-<<<<<<< Updated upstream
-             
-=======
              (define prop-val (tr:unit:body-exp-def-type-property stx))
              (cond
               [(equal? prop-val 'def/type) 
@@ -232,11 +229,7 @@
               [else 
                (define results (tc-expr stx))
                (define types (tc-results-ts results))
-               (printf "typed: ~a\n" types)])
->>>>>>> Stashed changes
-               
-               
-               )))
+               (printf "typed: ~a\n" types)]))))
      
      
      (printf "parsing ok!\n")
