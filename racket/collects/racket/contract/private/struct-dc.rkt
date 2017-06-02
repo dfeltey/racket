1705bee074f226d94467db2d5bd3493d99cf7f1a
Daniel Feltey <dfeltey@gmail.com>
2017-06-02 14:43:26 -0500

    Use appropriate value for struct logging

--- racket/collects/racket/contract/private/struct-dc.rkt
+++ racket/collects/racket/contract/private/struct-dc.rkt
@@ -1445,13 +1445,13 @@
                         (if (pred? #,(opt/info-val opt/info))
                             (begin
                               #,@s-fo-code
-                              (log-n-wrappers "opt-struct/dc" val)
+                              (log-n-wrappers "opt-struct/dc" #,(opt/info-val opt/info))
                               (chaperone-struct
                                #,(opt/info-val opt/info)
                                #,@(reverse s-chap-code) ;; built the last backwards, so reverse it here
                                stronger-prop-desc
                                (vector free-var ...)
-                               impersonator-prop:unwrapped val
+                               impersonator-prop:unwrapped #,(opt/info-val opt/info)
                                impersonator-prop:contracted #,(opt/info-contract opt/info)
                                impersonator-prop:blame #,(opt/info-blame opt/info)))
                             (struct/dc-error #,(opt/info-blame opt/info) #,(opt/info-val opt/info) 'struct-name)))))
